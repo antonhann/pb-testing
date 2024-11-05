ae8d0e4ba0b6cc6675fce943153a1661f659daf7
Anton Ha <antonha016@gmail.com>
2024-11-05 16:11:43 -0500

    added basic auth

--- pb_app/src/Auth.tsx
+++ pb_app/src/Auth.tsx
@@ -4,6 +4,7 @@ import {useForm} from "react-hook-form"
 export const Auth = () => {
     const {register, handleSubmit} = useForm();
     const [isLoading, setLoading] = useState<boolean>(false)
+    const isLoggedIn = pb.authStore.isValid;
     const login = async (data : any) => {
         setLoading(true)
         try{
@@ -16,6 +17,7 @@ export const Auth = () => {
     }
     const logOut = () => {
         pb.authStore.clear()
+        window.location.reload()
     }
     if (isLoading){
         return(
@@ -24,14 +26,21 @@ export const Auth = () => {
             </div>
         )
     }
+    if(isLoggedIn){
+        return(
+            <div>
+                <h1>Logged IN : {pb.authStore.isValid && pb.authStore.model?.email}</h1>
+                <button onClick={logOut}>logout</button>
+            </div>
+        )
+    }
     return(
         <div>
-            <h1>Logged IN : {pb.authStore.isValid && pb.authStore.model?.email}</h1>
+            <h1>Log In!</h1>
             <form onSubmit={handleSubmit(login)}>
                 <input type="text" placeholder="email" {...register("email")}/>
                 <input type="text" placeholder="password" {...register("password")}/>
                 <button type="submit" disabled = {isLoading}>{isLoading ? "loading" : "Login"}</button>
-                {pb.authStore.isValid && <button onClick={logOut}>logout</button>}
             </form>
         </div>
     )
